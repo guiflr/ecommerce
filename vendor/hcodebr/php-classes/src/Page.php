f4bcefe5d67ddef8737d0b878216425a7a724298
guiflr <guiflr97@gmail.com>
2019-01-01 22:09:45 -0200

    layoult

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -10,6 +10,8 @@ class Page{
 	private $tpl;
 	private $options = [];
 	private $default = [
+		"header"=>true,
+		"footer"=>true,
 		"data"=>[]
 	];
 
@@ -27,9 +29,14 @@ class Page{
 
 		$this->tpl = new Tpl();
 
-		$this->setData($this->options["data"]);
+		if($this->options["data"]) $this->setData($this->options["data"]);
 
-		$this->tpl->draw("header");
+		if($this->options["header"] === true) $this->tpl->draw("header");
+	}
+
+	public function __destruct(){
+
+		$this->tpl->draw("footer");
 	}
 
 	private function setData($data = array()){
@@ -38,7 +45,6 @@ class Page{
 
 			$this->tpl->assign($key, $value);
 		}
-
 	}
 
 	public function setTpl($name,$data = array(), $returnHTML = false){
@@ -47,11 +53,6 @@ class Page{
 
 		return $this->tpl->draw($name,$returnHTML);
 
-	}
-
-	public function __destruct(){
-
-		$this->tpl->draw("footer");
 	}
 }
 
