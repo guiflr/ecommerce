c07a1a098590d8248fe2dd9717b4cd1fd69cb326
guiflr <guiflr97@gmail.com>
2019-01-03 21:58:52 -0200

    Recuperação de senha

--- index.php
+++ index.php
@@ -31,6 +31,7 @@ $app->get('/admin', function() {
 
 });
 
+//INICIO LOGIN
 $app->get('/admin/login', function(){
 
 	$page = new PageAdm([
@@ -55,6 +56,153 @@ $app->get('/admin/logout', function(){
 	header("Location: /admin/login");
 	exit;
 });
+//FIM LOGIN
+
+//INICIO CRUD
+$app->get("/admin/users", function(){
+	
+	User::verifyLogin();
+
+	$users = User::listAll();
+
+	$page = new PageAdm();
+
+	$page->setTpl("users", array(
+		"users"=>$users
+	));
+});
+
+$app->get("/admin/users/create", function(){
+
+	User::verifyLogin();
+
+	$page = new PageAdm();
+
+	$page->setTpl("users-create");
+
+});
+
+$app->post("/admin/users/create", function(){
+	
+	User::verifyLogin();
+
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+	$user->setData($_POST);
+
+	$user->save();
+
+	header("Location: /admin/users");
+	exit;
+});
+
+$app->get("/admin/users/:iduser/delete", function($iduser){
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$user->delete();
+
+	header("Location: /admin/users");
+	exit;
+
+});
+
+$app->get("/admin/users/:iduser", function($iduser){
+	
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$page = new PageAdm();
+
+	$page->setTpl("users-update", array(
+		"user"=>$user->getData()
+	));
+});
+
+$app->post("/admin/users/:iduser", function($iduser){
+	
+	User::verifyLogin();	
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+	$user->setData($_POST);
+
+	$user->update();
+
+	header("Location: /admin/users");
+	exit;
+
+});
+//FIM CRUD
+
+//INICIO ESQUECEU A SENHA
+$app->get("/admin/forgot", function(){
+
+	$page = new PageAdm(["header="=>false,"footer"=>false]);
+
+	$page->setTpl("forgot");
+
+});
+
+$app->post("/admin/forgot", function(){
+
+	$user = User::getForgot($_POST["email"]);
+
+	header("Location: /admin/forgot/sent");
+	exit;
+});
+
+$app->get("/admin/forgot/sent", function(){
+
+	$page = new PageAdm(["header="=>false,"footer"=>false]);
+
+	$page->setTpl("forgot-sent");
+
+});
+
+$app->get("/admin/forgot/reset", function(){
+	
+	$user = User::validForgotDecrypt($_GET["code"]);
+
+	$page = new PageAdm(["header"=>false,"footer"=>false]);
+
+	$page->setTpl("forgot-reset", array(
+		"name"=>$user["desperson"],
+		"code"=>$_GET["code"]
+	));	
+});
+
+$app->post("/admin/forgot/reset", function(){
+
+	$forgot = User::validForgotDecrypt($_POST["code"]);
+
+	User::setForgotUsed($forgot["idrecovery"]);
+
+	$user = new User();
+
+	$user->get((int)$forgot["iduser"]);
+
+	$user->setPassword($_POST["password"]);
+
+	$page = new PageAdm(["header"=>false,"footer"=>false]);
+
+	$page->setTpl("forgot-reset-success");
+
+});
+
 
 $app->run();
 
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -4,6 +4,7 @@ namespace Hcode\Model;
 
 use Hcode\DB\Sql;
 use Hcode\Model;
+use Hcode\Mailer;
 
 Class User Extends Model{
 
@@ -22,7 +23,7 @@ Class User Extends Model{
 
 		$data = $res[0];
 
-		if(password_verify($password, $data["despassword"]) === true){
+		if($password === $data["despassword"]){
 			
 			$user = new User();
 
@@ -51,6 +52,178 @@ Class User Extends Model{
 	public static function logout(){
 		$_SESSION[User::SESSION] = NULL;
 	}
+
+	public static function listAll(){
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson");
+	}
+
+	public function save(){
+
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)",
+			array(
+				":desperson"=>$this->getdesperson(),
+				":deslogin"=>$this->getdeslogin(),
+				":despassword"=>$this->getdespassword(),
+				":desemail"=>$this->getdesemail(),
+				":nrphone"=>$this->getnrphone(),
+				":inadmin"=>$this->getinadmin()
+		));
+
+		$this->setData($results);
+	}
+
+	public function get($iduser){
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = :iduser",
+			array(
+			":iduser"=>$iduser
+		));
+
+		$this->setData($results[0]);
+	}
+
+	public function update(){
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)",
+			array(
+				":iduser"=>$this->getiduser(),
+				":desperson"=>$this->getdesperson(),
+				":deslogin"=>$this->getdeslogin(),
+				"despassword"=>$this->getdespassword(),
+				"desemail"=>$this->getdesemail(),
+				"nrphone"=>$this->getnrphone(),
+				"inadmin"=>$this->getinadmin()
+		));
+
+		$this->setData($results);
+	}
+
+	public function delete(){
+
+		$sql = new Sql();
+
+		$sql->query("CALL sp_users_delete(:iduser)", array(
+
+			":iduser"=>$this->getiduser()
+		));
+	}
+
+	public static function getForgot($email){
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_persons a INNER JOIN tb_users b USING(idperson) WHERE a.desemail = :email;", array(
+			":email"=>$email
+		));
+
+		if(count($results) === 0){
+
+			throw new \Exception("Senha não recuperada!");
+			
+		}else{
+
+			$data = $results[0];
+
+			$res2 = $sql->select("CALL sp_userspasswordsrecoveries_create(:iduser, :desip)", array(
+
+				":iduser"=>$data["iduser"],
+				":desip"=>$_SERVER["REMOTE_ADDR"]
+			));
+
+			if(count($res2) === 0){
+
+				throw new \Exception("Não foi possivel recuperar a senha!");
+				
+			}else{
+
+				$dataRecovery = $res2[0];
+
+				$code = $dataRecovery["idrecovery"];
+
+
+				$link = "http://dev.com/admin/forgot/reset?code=$code";
+
+				$mailer = new Mailer($data["desemail"], $data["desperson"], "Redefinir a senha do E-commerce", "forgot", array(
+					"name"=>$data["desperson"],
+					"link"=>$link
+				));
+
+				$mailer->send();
+
+				return $data;
+
+
+			}
+		}
+	}
+
+	public function validForgotDecrypt($code){
+		$idrecovery = $code;
+
+		$sql = new Sql();
+
+		$results = $sql->select("
+
+				SELECT * FROM tb_userspasswordsrecoveries a
+				INNER JOIN tb_users b USING(iduser)
+				INNER JOIN tb_persons c USING(idperson)
+				WHERE
+					a.idrecovery = :idrecovery
+					AND
+					a.dtrecovery IS NULL
+					AND
+					DATE_ADD(a.dtregister, INTERVAL 1 HOUR) >= NOW();
+
+			", array(
+				"idrecovery"=>$idrecovery
+			));
+
+			if(count($results) === 0){
+
+				throw new \Exception("Senha nao recuperada");
+				
+			}else{
+				return $results[0];
+			}
+	}
+
+	public static function setForgotUsed($idrecovery){
+
+		$sql = new Sql();
+
+		$sql->query("UPDATE tb_userspasswordsrecoveries SET dtrecovery = NOW() WHERE idrecovery = :idrecovery", array(
+
+				":idrecovery"=>$idrecovery
+		));
+	}
+
+	public function setPassword($password){
+
+		$sql = new Sql();
+
+		$sql->query("UPDATE tb_users SET despassword = :despassword WHERE iduser = :iduser", array(
+
+			":despassword"=>$password,
+			":iduser"=>$this->getiduser()
+		));
+
+	}	
 }
 
 ?>
+
+
+
+
+
+
+
+
