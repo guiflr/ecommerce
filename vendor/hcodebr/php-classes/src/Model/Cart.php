9bd0e50396a6ac671c75719b0f5353a33aaf2df3
guiflr <guiflr97@gmail.com>
2019-01-10 21:27:46 -0200

    Produtos no carrinho

--- vendor/hcodebr/php-classes/src/Model/Cart.php
+++ vendor/hcodebr/php-classes/src/Model/Cart.php
@@ -84,16 +84,60 @@ Class Cart Extends Model{
 
 		$sql = new Sql();
 
-		$result = $sql->select("CALL sp_carts_save(:idcart, :dessessionid, :iduser, :deszipcode, :vlfreight, :nrdays)",[
+		$result = $sql->select("CALL sp_carts_save(:idcart, :dessessionid, :iduser, :idaddress, :vlfreight,:dtregister)",[
 			":idcart"=>$this->getidcart(),
 			":dessessionid"=>$this->getdessessionid(),
 			":iduser"=>$this->getiduser(),
-			":deszipcode"=>$this->getdeszipcode(),
+			":idaddress"=>$this->getidaddress(),
 			":vlfreight"=>$this->getvlfreight(),
-			":nrdays"=>$this->getnrdays()
+			":dtregister"=>$this->getdtregister()
 		]);
 
 		$this->setData($result[0]);
+	}
+
+	public function addProduct(Product $product){
+
+		$sql = new Sql();
+
+		$sql->query("INSERT INTO tb_cartsproducts(idcart,idproduct) VALUES(:idcart,:idproduct)", [
+			":idcart"=>$this->getidcart(),
+			":idproduct"=>$product->getidproduct()
+		]);
+	}
+
+	public function removeProduct(Product $product, $all = false){
+
+		$sql = new Sql();
+
+		if($all){
+
+			$sql->query("UPDATE tb_cartsproducts SET dtremoved = NOW() WHERE idcart = :idcart AND idproduct = :idproduct AND dtremoved IS NULL", [
+				":idcart"=>$this->getidcart(),
+				":idproduct"=>$product->getidproduct()
+			]);	
+		}else{
+			$sql->query("UPDATE tb_cartsproducts SET dtremoved = NOW() WHERE idcart = :idcart AND idproduct = :idproduct AND dtremoved IS NULL LIMIT 1", [
+				":idcart"=>$this->getidcart(),
+				":idproduct"=>$product->getidproduct()
+			]);
+		}
+		
+	}
+
+	public function getProducts(){
+
+		$sql = new Sql();
+		$res =  $sql->select("SELECT b.idproduct,b.desproduct, b.vlprice,b.vlwidth,b.vlheight,b.vllength,b.vlweight,b.desurl, COUNT(*) AS nrqtd, SUM(b.vlprice) AS vltotal 
+			FROM tb_cartsproducts a
+			INNER JOIN tb_products b
+			ON a.idproduct = b.idproduct
+			WHERE a.idcart = :idcart AND a.dtremoved IS NULL
+			GROUP BY b.idproduct,b.desproduct, b.vlprice,b.vlwidth,b.vlheight,b.vllength,b.vlweight,b.desurl
+			ORDER BY b.desproduct",[
+				":idcart"=>$this->getidcart()
+			]);
+		return Product::checkList($res);
 	}
 }
 
